d7c5e23bc5c7eeda51dd8c957a88a87ed4868714
tyler <tylerjstefani@gmail.com>
2025-04-10 18:05:58 -0500

    disable crash dump

--- hosts/nixos/homeserver/configuration.nix
+++ hosts/nixos/homeserver/configuration.nix
@@ -20,7 +20,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_6_1;
-  boot.crashDump.enable = true;
 
   environment.systemPackages = with pkgs; [
     git
