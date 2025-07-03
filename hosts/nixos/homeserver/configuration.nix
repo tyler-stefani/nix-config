02262ad0944346a0bb43ad577b144a39f7637e26
tyler <tylerjstefani@gmail.com>
2025-07-03 00:24:37 -0500

    configure host for home server

--- hosts/nixos/homeserver/configuration.nix
+++ hosts/nixos/homeserver/configuration.nix
@@ -23,7 +23,13 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_6_1;
 
-  hostConfig.hostName = "homeserver";
+  hostConfig = {
+    hostName = "homeserver";
+    directories = {
+      personalData = /home/tyler/shared/safe/data;
+      appData = /home/tyler/shared/safe/apps;
+    };
+  };
 
   networking = {
     networkmanager.enable = true;
