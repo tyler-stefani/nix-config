9940baa7b4fc3b4aef71d3607f8ec5610078f336
tyler <tylerjstefani@gmail.com>
2025-10-22 23:40:40 -0500

    update home configuration attrs to align with nh defaults

--- home/users/tyler/default.nix
+++ home/users/tyler/default.nix
@@ -1,21 +1,25 @@
 { inputs, config, ... }:
-{
-  flake.homeConfigurations = builtins.listToAttrs (
-    builtins.map (system: {
-      name = "tyler@${system}";
-      value = inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = import inputs.nixpkgs { inherit system; };
-        extraSpecialArgs = {
-          traits = config.flake.homeTraits;
-        };
-        modules = [
-          inputs.nixvim.homeModules.nixvim
-          inputs.stylix.homeModules.stylix
-
-          ./configuration.nix
-        ]
-        ++ builtins.attrValues config.flake.homeModules;
+let
+  mkHomeConfig =
+    system:
+    inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = import inputs.nixpkgs { inherit system; };
+      extraSpecialArgs = {
+        traits = config.flake.homeTraits;
       };
-    }) config.systems
-  );
+      modules = [
+        inputs.nixvim.homeModules.nixvim
+        inputs.stylix.homeModules.stylix
+
+        ./configuration.nix
+      ]
+      ++ builtins.attrValues config.flake.homeModules;
+    };
+in
+{
+  flake.homeConfigurations = {
+    "tyler@bloob" = mkHomeConfig "x86_64-linux";
+    "tyler@homeserver" = mkHomeConfig "x86_64-linux";
+    "tyler@noodle" = mkHomeConfig "x86_64-darwin";
+  };
 }
