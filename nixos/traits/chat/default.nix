79079e501ba3fdee83d317a0f4a95253371886f5
tyler <tylerjstefani@gmail.com>
2026-02-23 23:28:44 -0600

    create chat and call traits with matrix and element

---
+++ nixos/traits/chat/default.nix
@@ -0,0 +1,23 @@
+{ ... }:
+{
+  flake.nixosTraits.chat =
+    { config, mounts, ... }:
+    {
+      sops.envs.chat = {
+        sopsFile = ./secrets/.env;
+      };
+      virtualisation.docker-compose.chat =
+        let
+          dataDir = "${mounts.data}/chat";
+          configDir = "${mounts.config}/matrix";
+        in
+        {
+          file = ./docker-compose.yaml;
+          env = {
+            DATA_DIR = dataDir;
+            CONFIG_DIR = configDir;
+          };
+          envPath = config.sops.envs.chat.path;
+        };
+    };
+}
