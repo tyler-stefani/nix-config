6f4c6c46a738a6181f1b8c9cd192e57df80ad32e
tyler <tylerjstefani@gmail.com>
2026-02-25 00:10:38 -0600

    add netbird setup key

--- nixos/traits/mesh-vpn/default.nix
+++ nixos/traits/mesh-vpn/default.nix
@@ -1,8 +1,20 @@
 { ... }:
 {
   flake.nixosTraits.mesh-vpn =
-    { ... }:
+    { config, ... }:
     {
       services.tailscale.enable = true;
+
+      sops.secrets.mesh-vpn = {
+        sopsFile = ./secrets/key.yaml;
+        key = "key";
+      };
+
+      services.netbird.clients.default = {
+        login = {
+          enable = true;
+          setupKeyFile = "${config.sops.secrets.mesh-vpn.path}";
+        };
+      };
     };
 }
