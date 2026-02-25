f1d6418b5efee3fcad4aa07105c4df5dfa6b581d
tyler <tylerjstefani@gmail.com>
2026-02-24 23:19:13 -0600

    create vpn management service with netbird

---
+++ nixos/traits/mesh-vpn-manager/default.nix
@@ -0,0 +1,27 @@
+{ ... }:
+{
+  flake.nixosTraits.mesh-vpn-manager =
+    { config, mounts, ... }:
+    {
+      sops.envs = {
+        mesh-vpn-dash = {
+          sopsFile = ./secrets/dashboard.env;
+        };
+        mesh-vpn-proxy = {
+          sopsFile = ./secrets/proxy.env;
+        };
+      };
+      virtualisation.docker-compose.mesh-vpn-manager = {
+        file = ./docker-compose.yml;
+        env = {
+          DASHBOARD_ENV_PATH = config.sops.envs.mesh-vpn-dash.path;
+          PROXY_ENV_PATH = config.sops.envs.mesh-vpn-proxy.path;
+          CONFIG_PATH = "${mounts.config}/netbird";
+        };
+      };
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
+    };
+}
