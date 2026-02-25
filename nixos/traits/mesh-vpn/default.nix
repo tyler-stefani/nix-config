f499c35aa25dd8bcb79814e82c3e96e148185b65
tyler <tylerjstefani@gmail.com>
2026-02-25 01:12:17 -0600

    add missing wireguard port

--- nixos/traits/mesh-vpn/default.nix
+++ nixos/traits/mesh-vpn/default.nix
@@ -10,11 +10,15 @@
         key = "key";
       };
 
-      services.netbird.clients.default = {
-        login = {
-          enable = true;
-          setupKeyFile = "${config.sops.secrets.mesh-vpn.path}";
-        };
+      services.netbird.clients.wt0 = {
+        port = 51820;
+        # login is not working on this version
+        # - there is currently no way to set the management url
+        # - the docker sock opened does not line up with the default for netbird up
+        # login = {
+        #   enable = true;
+        #   setupKeyFile = "${config.sops.secrets.mesh-vpn.path}";
+        # };
       };
     };
 }
