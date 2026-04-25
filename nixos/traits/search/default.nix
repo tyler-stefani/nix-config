f266c2b059762636af1636f530a53555f9929ca6
tyler <tylerjstefani@gmail.com>
2026-04-24 22:11:37 -0500

    switch search app to degoog

--- nixos/traits/search/default.nix
+++ nixos/traits/search/default.nix
@@ -9,9 +9,7 @@
       virtualisation.docker-stack.search = {
         file = ./docker-compose.yaml;
         env = {
-          CONFIG_DIR = "${mounts.config}/searxng/config";
-          DATA_DIR = "${mounts.config}/searxng/data";
-          VALKEY_DATA_DIR = "${mounts.config}/searxng/valkey-data";
+          DATA_DIR = "${mounts.config}/degoog";
         };
         envPath = config.sops.envs.search.path;
       };
