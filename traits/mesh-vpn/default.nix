9777a9fdedda1f2414c7bb2f6dcbbd7a827ebd99
tyler <tylerjstefani@gmail.com>
2026-07-23 19:20:00 -0500

    add additional secrets for mesh-vpn

--- traits/mesh-vpn/default.nix
+++ traits/mesh-vpn/default.nix
@@ -10,9 +10,13 @@
         mesh-vpn-proxy = {
           sopsFile = ./secrets/proxy.env;
         };
+        mesh-vpn-domain = {
+          sopsFile = ./secrets/domain.env;
+        };
       };
       virtualisation.docker-compose.mesh-vpn = {
         file = ./docker-compose.yml;
+        envPath = config.sops.envs.mesh-vpn-domain.path;
         env = {
           DASHBOARD_ENV_PATH = config.sops.envs.mesh-vpn-dash.path;
           PROXY_ENV_PATH = config.sops.envs.mesh-vpn-proxy.path;
